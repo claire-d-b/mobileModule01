0b6c1b9ae79e67c7fc4153bd252e8a361804a39e
claire-d-b <claire.deberny@essca.eu>
2026-06-24 13:21:07 +0200

    light theme for react native paper

--- weather_app/app/index.tsx
+++ weather_app/app/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import CAppbar from "./CAppbar";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <PaperProvider>
+      <PaperProvider theme={MD3LightTheme}>
         <CAppbar />
       </PaperProvider>
     </SafeAreaProvider>
